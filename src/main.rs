f6a26d0c4362f7c64397d6b669725773987afba3
elapuestojoe <kevin.islas.abud@gmail.com>
2022-09-17 16:50:32 -0700

    Adds shading w normal map

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,24 @@ use r_ray::ray::Ray;
 use r_vector::vector::Vector;
 use r_vector::vector::VectorOperations;
 
-fn hit_sphere(center: &Vector<f32>, radius: f32, ray: &Ray<f32>) -> bool {
+fn hit_sphere(center: &Vector<f32>, radius: f32, ray: &Ray<f32>) -> f32 {
     let oc = ray.origin() - center;
 
     let a = ray.direction().dot(&ray.direction());
     let b = 2.0 * oc.dot(ray.direction());
     let c = oc.dot(&oc) - (radius * radius);
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        return -1.0;
+    }
+    (-b - discriminant.sqrt()) / (2.0 * a)
 }
 
 fn color(ray: &Ray<f32>) -> Vector<f32> {
-    if hit_sphere(&Vector::<f32>::new(0.0, 0.0, -1.0), 0.5, ray) {
-        return Vector::<f32>::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(&Vector::<f32>::new(0.0, 0.0, -1.0), 0.5, ray);
+    if t > 0.0 {
+        let normal = (&ray.point_at_time(t) - &Vector::<f32>::new(0.0, 0.0, -1.0)).unit_vector();
+        return Vector::<f32>::new(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0) * 0.5;
     }
     let unit_direction = ray.direction().unit_vector();
     let t = (&unit_direction.y() + 1.0) * 0.5;
@@ -42,9 +47,9 @@ fn main() {
             );
             let color = color(&ray);
 
-            let ir = (255.99 * color.r()).floor() as i32;
-            let ig = (255.99 * color.g()).floor() as i32;
-            let ib = (255.99 * color.b()).floor() as i32;
+            let ir = (255.99 * color.r()) as i32;
+            let ig = (255.99 * color.g()) as i32;
+            let ib = (255.99 * color.b()) as i32;
 
             println!("{} {} {}\n", ir, ig, ib);
         }
