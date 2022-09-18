1c13c36a56a7c1d0d673a1109f95fe87baba237c
elapuestojoe <kevin.islas.abud@gmail.com>
2022-09-18 16:48:40 -0700

    Adds difuse materials

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,55 @@ use r_vector::vector::Vector;
 use r_vector::vector::VectorOperations;
 use rand::Rng;
 
-fn get_color(ray: &Ray<f32>, world: &HittableList<f32>) -> Vector<f32> {
+mod randutil {
+    use r_vector::vector::Vector;
+    use rand::Rng;
+
+    fn rand_f32_range(low: f32, high: f32) -> f32 {
+        let mut rng = rand::thread_rng();
+        rng.gen_range(low, high)
+    }
+
+    #[allow(dead_code)]
+    fn rand_f32() -> f32 {
+        let mut rng = rand::thread_rng();
+        rng.gen_range(0.0, 1.0)
+    }
+
+    #[allow(dead_code)]
+    fn random_vec() -> Vector<f32> {
+        Vector::<f32>::new(rand_f32(), rand_f32(), rand_f32())
+    }
+
+    fn random_vec_range(low: f32, high: f32) -> Vector<f32> {
+        Vector::<f32>::new(
+            rand_f32_range(low, high),
+            rand_f32_range(low, high),
+            rand_f32_range(low, high),
+        )
+    }
+
+    pub fn random_in_unit_sphere() -> Vector<f32> {
+        let mut point = random_vec_range(-1.0, 1.0);
+        while point.length_squared() >= 1.0 {
+            point = random_vec_range(-1.0, 1.0);
+        }
+        point
+    }
+}
+
+fn ray_color(ray: &Ray<f32>, world: &HittableList<f32>) -> Vector<f32> {
     let mut hit_record = HitRecord::<f32>::new();
+
     if world.hit(&ray, 0.0, f32::MAX, &mut hit_record) {
-        return Vector::<f32>::new(
-            hit_record.normal.x() + 1.0,
-            hit_record.normal.y() + 1.0,
-            hit_record.normal.z() + 1.0,
+        let target =
+            &hit_record.point_at_t + &hit_record.normal + &randutil::random_in_unit_sphere();
+        return ray_color(
+            &Ray::new(
+                hit_record.point_at_t.clone(),
+                &target - &hit_record.point_at_t,
+            ),
+            world,
         ) * 0.5;
     }
 
@@ -47,7 +89,7 @@ fn main() {
                 let u = ((i as f32) + rng.gen_range(0.0f32, 1.0f32)) / (nx as f32);
                 let v = ((j as f32) + rng.gen_range(0.0f32, 1.0f32)) / (ny as f32);
                 let ray = camera.get_ray(u, v);
-                color += get_color(&ray, &world);
+                color += ray_color(&ray, &world);
             }
             color /= ns as f32;
             let ir = (255.99 * color.r()) as i32;
