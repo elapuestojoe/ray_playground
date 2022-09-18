0984a9596b46cad05eadf78205df40d04354b95a
elapuestojoe <kevin.islas.abud@gmail.com>
2022-09-18 13:59:16 -0700

    Uses hittableList to draw two spheres :)

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,21 @@
+use r_ray::geometry::sphere::Sphere;
+use r_ray::hittable::hittable::HitRecord;
+use r_ray::hittable::hittable::Hittable;
+use r_ray::hittable::hittable::HittableList;
 use r_ray::ray::Ray;
 use r_vector::vector::Vector;
 use r_vector::vector::VectorOperations;
 
-fn hit_sphere(center: &Vector<f32>, radius: f32, ray: &Ray<f32>) -> f32 {
-    let oc = ray.origin() - center;
-
-    let a = ray.direction().dot(&ray.direction());
-    let b = 2.0 * oc.dot(ray.direction());
-    let c = oc.dot(&oc) - (radius * radius);
-    let discriminant = b * b - 4.0 * a * c;
-    if discriminant < 0.0 {
-        return -1.0;
+fn color(ray: &Ray<f32>, world: &HittableList<f32>) -> Vector<f32> {
+    let mut hit_record = HitRecord::<f32>::new();
+    if world.hit(&ray, 0.0, f32::MAX, &mut hit_record) {
+        return Vector::<f32>::new(
+            hit_record.normal.x() + 1.0,
+            hit_record.normal.y() + 1.0,
+            hit_record.normal.z() + 1.0,
+        ) * 0.5;
     }
-    (-b - discriminant.sqrt()) / (2.0 * a)
-}
 
-fn color(ray: &Ray<f32>) -> Vector<f32> {
-    let t = hit_sphere(&Vector::<f32>::new(0.0, 0.0, -1.0), 0.5, ray);
-    if t > 0.0 {
-        let normal = (&ray.point_at_time(t) - &Vector::<f32>::new(0.0, 0.0, -1.0)).unit_vector();
-        return Vector::<f32>::new(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0) * 0.5;
-    }
     let unit_direction = ray.direction().unit_vector();
     let t = (&unit_direction.y() + 1.0) * 0.5;
     Vector::<f32>::new(1.0, 1.0, 1.0) * (1.0 - t) + (Vector::<f32>::new(0.5, 0.7, 1.0) * t)
@@ -36,6 +31,10 @@ fn main() {
     let vertical = Vector::<f32>::new(0.0, 2.0, 0.0);
     let origin = Vector::<f32>::new(0.0, 0.0, 0.0);
 
+    let sphere_1 = Box::new(Sphere::<f32>::new(Vector::new(0.0, 0.0, -1.0), 0.5));
+    let sphere_2 = Box::new(Sphere::<f32>::new(Vector::new(0.0, -100.5, -1.0), 100.0));
+    let world = HittableList::new(vec![sphere_1, sphere_2]);
+
     for j in (0..ny).rev() {
         for i in 0..nx {
             let u = (i as f32) / (nx as f32);
@@ -45,7 +44,7 @@ fn main() {
                 origin.clone(),
                 &lower_left_corner + &(&horizontal * u) + (&vertical * v),
             );
-            let color = color(&ray);
+            let color = color(&ray, &world);
 
             let ir = (255.99 * color.r()) as i32;
             let ig = (255.99 * color.g()) as i32;
